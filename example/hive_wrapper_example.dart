b057092a60947d25417523d90156c0efc6f3a2c7
hamed <hamed.hp332@gmail.com>
2021-04-05 01:03:00 +0430

    init

---
+++ example/hive_wrapper_example.dart
@@ -0,0 +1,6 @@
+import 'package:hive_wrapper/hive_wrapper.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/hive_wrapper_test.dart
@@ -0,0 +1,16 @@
+import 'package:hive_wrapper/hive_wrapper.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    Awesome awesome;
+
+    setUp(() {
+      awesome = Awesome();
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
